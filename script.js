d8ffbece3fffcb4d1310794228feed3993ce87aa
StefanAmur <stefan.amur22@gmail.com>
2021-11-18 16:03:26 +0100

    added basic obstacle that moves opposite of the player

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let keys = [];
 let tic = 20;
 let x = 0;
+let obstacleX = 0;
 let playerY = 137;
 let jumpValue = 80;
 let player = document.getElementById('player');
@@ -8,6 +9,7 @@ let ground = document.getElementById('background5');
 let closeTrees = document.getElementById('background4');
 let distantTrees = document.getElementById('background3');
 let clouds = document.getElementById('background2');
+let obstacle = document.getElementById('obstacle');
 
 move();
 jump();
@@ -34,15 +36,15 @@ document.addEventListener('keyup', function (event) {
 function move() {
     if (keys.includes('ArrowRight')) {
         x -= 1;
+        obstacleX -= 1;
         player.style.transform = 'scaleX(1)';
     }
     if (keys.includes('ArrowLeft')) {
         x += 1;
+        obstacleX += 1;
         player.style.transform = 'scaleX(-1)';
     }
-    if (keys.includes('ArrowUp')) {
-        player.style.backgroundImage = 'url(./img/jump.gif)';
-    }
+    obstacle.style.left = 4 * obstacleX + 'px';
     ground.style.backgroundPositionX = 4 * x + "px";
     closeTrees.style.backgroundPositionX = 3 * x + "px";
     distantTrees.style.backgroundPositionX = 2 * x + "px";
@@ -53,10 +55,8 @@ function move() {
 function checkInput() {
     if (keys.length != 0 && (keys.includes('ArrowLeft') || keys.includes('ArrowRight')) && !keys.includes('ArrowUp')) {
         player.style.backgroundImage = 'url(./img/run.gif)';
-        console.log('left or right');
     } else if (keys.includes('ArrowUp')) {
         player.style.backgroundImage = 'url(./img/jump.gif)';
-        console.log('space');
     } else {
         player.style.backgroundImage = 'url(./img/idle.gif)';
     }
@@ -65,13 +65,12 @@ function checkInput() {
 
 function jump() {
     if (keys.includes('ArrowUp')) {
-        console.log('jump func triggered');
         player.style.backgroundImage = 'url(./img/jump.gif)';
         player.style.bottom = playerY + jumpValue + 'px';
         player.style.transition = 'bottom 0.5s';
     } else if (!keys.includes('ArrowUp')) {
         player.style.bottom = playerY + 'px';
-        player.style.transition = 'bottom 0';
+        player.style.transition = 'bottom 0.5s';
     }
     setTimeout(jump, tic);
 }
